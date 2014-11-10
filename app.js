1f4e0e458ea1049b181e2996d009b0c64de1ceed
sterlingcrispin <sterlingcrispin@gmail.com>
2014-11-09 17:02:47 -0800

    removed mobile forwarding test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var thehight = $(window).height();
 $("section").css("height", thehight);
 
 
+
 $(function() {
     $("#zero").waypoint(function(direction) {
         $("#downarrowlink").attr("href", "#one");
